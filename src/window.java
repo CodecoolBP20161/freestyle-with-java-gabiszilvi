8a112fb8a5f10c9388a41fb4221e0263a8b7bd9b
Lőrincz Szilvia <lorszil@Lorinczs-MacBook-Pro.local>
2016-11-02 11:16:18 +0100

    we have a window :O

---
+++ src/window.java
@@ -0,0 +1,29 @@
+/**
+ * Created by lorszil on 02/11/16.
+ */
+
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class window extends JFrame{
+
+    private static void window(){
+        JFrame frame = new JFrame("Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(1024, 768));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    };
+
+    public static void main(String args[]){
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run() {
+                window();
+            }
+        });
+    };
+}
